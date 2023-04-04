99d24592e4ea75ef544c16f8dbc5f78cde0606a7
André Chaves Teixeira <TRI0106062@aluno.firjansenaisesi.com.br>
2023-04-03 21:17:11 -0300

    debugg

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,37 +10,27 @@ export class AuthService {
         private readonly userService: UserService,
         private jwtService: JwtService
     ) {
-
     }
     generateJwt(payload) {
         return this.jwtService.sign(payload, { secret: process.env.JWT_SECRET });
     }
-
     async getUserDataFromGoogle(code: string) {
-
         const result = await fetch(`${process.env.GOOGLE_PROFILE_URL}${code}`);
-
         return await result.json();
-
-
     }
 
-
     async singIn(user: UserEntity) {
-
         var userLocal: UserEntity;
         var token = '',
-
             userLocal = await this.userService.findOneByEmail(user.email);
         if (!userLocal) {
             userLocal = await this.signUp(user);
-
-        } 
-        
+        }
         token = JSON.parse(JSON.stringify(userLocal));
-        token['exp'] = new Date().getUTCDate() + 1;
+        var date = new Date();
+        date.setUTCHours(date.getUTCHours() + 1);
+        token['exp'] = Math.floor(date.getTime() / 1000);
         return this.generateJwt(token);
-
     }
     async signUp(user: UserEntity): Promise<UserEntity> {
         const newUser = await this.userService.create(user);
